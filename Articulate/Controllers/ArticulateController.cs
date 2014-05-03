4abb80f055fdb932638b9c021ba5af795416d6a8
Shannon <sdeminick@gmail.com>
2014-05-03 20:08:00 +1000

    committing code that can create a fake node - will undo this though as I won't use it but is still neat

--- Articulate/Controllers/ArticulateController.cs
+++ Articulate/Controllers/ArticulateController.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Linq;
 using System.Web.Mvc;
+using Articulate.Models;
 using Umbraco.Core;
 using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
 
 namespace Articulate.Controllers
 {
+    public class ArticulateTagsController : RenderMvcController
+    {
+        public override ActionResult Index(RenderModel model)
+        {
+            var post = new ListModel(model.Content);
+            return View(PathHelper.GetThemeViewPath(post, "List"), post);
+        }
+    }
+
     public class ArticulateController : ArticulateListController
     {
         public override ActionResult Index(RenderModel model)
--- Articulate/UmbracoEventHandler.cs
+++ Articulate/UmbracoEventHandler.cs
@@ -2,21 +2,67 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Web.Routing;
+using Articulate.Models;
+using umbraco;
 using Umbraco.Core;
 using Umbraco.Core.Services;
 using Umbraco.Web;
+using Umbraco.Web.Routing;
 
 namespace Articulate
 {
+    public class TagContentFinder : ContentFinderByNiceUrl
+    {
+        public override bool TryFindContent(PublishedContentRequest contentRequest)
+        {
+            var route = !contentRequest.HasDomain ? contentRequest.Uri.GetAbsolutePathDecoded() : contentRequest.Domain.RootNodeId.ToString() + DomainHelper.PathRelativeToDomain(contentRequest.DomainUri, contentRequest.Uri.GetAbsolutePathDecoded());
+
+            var parts = route.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length > 0)
+            {   
+                //if it ends with '/tags'
+                if (parts[parts.Length - 1].InvariantEquals("tags"))
+                {
+                    route = route.Substring(0, route.LastIndexOf("/tags", StringComparison.InvariantCultureIgnoreCase) + 1);
+                    var parent = FindContent(contentRequest, route);
+                    if (parent != null && parent.DocumentTypeAlias.InvariantEquals("Articulate"))
+                    {
+                        //ok, so we want to render a tag page
+                        contentRequest.PublishedContent = new TagPage();
+                        return true;
+                    }
+                }    
+                //if the 2nd last item is /tags
+                if (parts.Length > 1 && parts[parts.Length - 2].InvariantEquals("tags"))
+                {
+                    
+                }
+            }
+            
+            
+            var byNiceUrl = new ContentFinderByNiceUrl();
+
+            return false;
+        }
+    }
+
     public class UmbracoEventHandler : ApplicationEventHandler
     {
+        protected override void ApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            ContentFinderResolver.Current
+                .InsertType<TagContentFinder>();
+        }
+
         protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
         {
             ContentService.Created += ContentService_Created;
         }
 
-        void ContentService_Created(IContentService sender, Umbraco.Core.Events.NewEventArgs<Umbraco.Core.Models.IContent> e)
+        static void ContentService_Created(IContentService sender, Umbraco.Core.Events.NewEventArgs<Umbraco.Core.Models.IContent> e)
         {
             if (UmbracoContext.Current != null)
             {
