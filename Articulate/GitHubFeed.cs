d1eabec3b6686a6b1f2e8a0ed0e3ae482ffd8d34
Shannon <sdeminick@gmail.com>
2014-11-17 19:17:03 +1100

    Updating shazwazza theme, updating packages

---
+++ Articulate/GitHubFeed.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Umbraco.Core.Cache;
+
+namespace Articulate
+{
+    public sealed class GitHubFeed
+    {
+        private readonly IRuntimeCacheProvider _cache;
+        private readonly string _url;
+        private readonly int _maxResults;
+
+        public GitHubFeed(IRuntimeCacheProvider cache, string feedUrl, int maxResults = 10)
+        {
+            _cache = cache;
+            _url = feedUrl;
+            _maxResults = maxResults;
+        }
+
+        public string[] GetResult()
+        {
+            return (string[])_cache.GetCacheItem(typeof(GitHubFeed).ToString(), () =>
+            {
+                using (var client = new HttpClient())
+                {
+                    var result = client.GetStringAsync(_url);
+                    Task.WaitAll(result);
+                    var xml = XDocument.Parse(result.Result);
+                    var ns = XNamespace.Get("http://www.w3.org/2005/Atom");
+
+                    return xml.Root.Descendants(ns + "content").Select(x => x.Value).Take(_maxResults).ToArray();
+                }
+            }, TimeSpan.FromHours(0.5));
+        }
+    }
+}
---
+++ Articulate/JsonFeedHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Umbraco.Core.Cache;
+
+namespace Articulate
+{
+    public static class JsonFeedHelper
+    {
+        public static JArray GetResult(IRuntimeCacheProvider cache, string url)
+        {
+            return (JArray)cache.GetCacheItem(url, () =>
+            {
+                using (var client = new HttpClient())
+                {
+                    var result = client.GetStringAsync(url);
+                    Task.WaitAll(result);
+                    return JsonConvert.DeserializeObject<JArray>(result.Result);
+                }
+            });
+
+        }
+    }
+}
