26c2c952c9f8d16619db6dc650f0abd4d7f83599
Andy Butland <abutland@zonecontent.com>
2015-06-20 23:07:48 +0200

    Link to author page from post

--- src/Articulate/Models/PostModel.cs
+++ src/Articulate/Models/PostModel.cs
@@ -74,6 +74,8 @@ namespace Articulate.Models
                             _author.Image = !imageVal.IsNullOrWhiteSpace()
                                 ? authorNode.GetCropUrl(propertyAlias: "authorImage", imageCropMode: ImageCropMode.Max) 
                                 : null;
+
+                            _author.BlogUrl = authorNode.Url;
                         }
                     }
                 }
