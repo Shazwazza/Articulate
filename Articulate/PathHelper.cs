8ee5da2e9baafca7d501182e5f977198d9016ddc
Shannon <sdeminick@gmail.com>
2015-07-18 16:03:15 +0200

    Updates to latest Umbraco and adds some exceptions to path helper

--- Articulate/PathHelper.cs
+++ Articulate/PathHelper.cs
@@ -1,4 +1,6 @@
-﻿using Articulate.Models;
+﻿using System;
+using Articulate.Models;
+using Umbraco.Core;
 
 namespace Articulate
 {
@@ -6,18 +8,33 @@ namespace Articulate
     {
         public static string GetThemePath(IMasterModel model)
         {
+            if (model.Theme.IsNullOrWhiteSpace())
+            {
+                throw new InvalidOperationException("No theme has been set for this Articulate root, republish the root with a selected theme");
+            }
+
             var path = "~/App_Plugins/Articulate/Themes/{0}/";
             return string.Format(path, model.Theme);
         }
 
         public static string GetThemeViewPath(IMasterModel model, string viewName)
         {
+            if (model.Theme.IsNullOrWhiteSpace())
+            {
+                throw new InvalidOperationException("No theme has been set for this Articulate root, republish the root with a selected theme");
+            }
+
             var path = "~/App_Plugins/Articulate/Themes/{0}/Views/{1}.cshtml";
             return string.Format(path, model.Theme, viewName);
         }
 
         public static string GetThemePartialViewPath(IMasterModel model, string viewName)
         {
+            if (model.Theme.IsNullOrWhiteSpace())
+            {
+                throw new InvalidOperationException("No theme has been set for this Articulate root, republish the root with a selected theme");
+            }
+
             var path = "~/App_Plugins/Articulate/Themes/{0}/Views/Partials/{1}.cshtml";
             return string.Format(path, model.Theme, viewName);
         }
