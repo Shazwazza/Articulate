e7a315193cc7a415896c2e2386b46975228fdd11
Shannon <sdeminick@gmail.com>
2016-03-18 15:07:28 +0100

    Cleans up the PR code, fixees a few issues - Decoupling Examine search from ArticulateSearchController #108

---
+++ src/Articulate/Extensions/IArticulateSearcher.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Umbraco.Core.Models;
+
+namespace Articulate.Extensions
+{
+    public interface IArticulateSearcher
+    {
+        IEnumerable<IPublishedContent> Search(string term, string provider, int blogArchiveNodeId);
+    }
+}
