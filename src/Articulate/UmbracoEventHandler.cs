ebf7b4f4d7f48f02f950b82400c6fc221f0a32dd
Shannon <sdeminick@gmail.com>
2016-07-04 13:21:01 +0200

    ensure routes are rebuilt when domains are refreshed

--- src/Articulate/UmbracoEventHandler.cs
+++ src/Articulate/UmbracoEventHandler.cs
@@ -55,6 +55,13 @@ namespace Articulate
             ServerVariablesParser.Parsing += ServerVariablesParser_Parsing;
             ContentTypeService.SavingContentType += ContentTypeService_SavingContentType;
             PageCacheRefresher.CacheUpdated += PageCacheRefresher_CacheUpdated;
+            DomainCacheRefresher.CacheUpdated += DomainCacheRefresher_CacheUpdated;
+        }
+
+        private void DomainCacheRefresher_CacheUpdated(DomainCacheRefresher sender, Umbraco.Core.Cache.CacheRefresherEventArgs e)
+        {
+            //ensure routes are rebuilt
+            ApplicationContext.Current.ApplicationCache.RequestCache.GetCacheItem("articulate-refresh-routes", () => true);
         }
 
         /// <summary>
@@ -105,7 +112,7 @@ namespace Articulate
                     var item = UmbracoContext.Current.ContentCache.GetById((int) e.MessageObject);
                     if (item != null && item.DocumentTypeAlias.InvariantEquals("Articulate"))
                     {
-                        //add the unpublished entities to the request cache
+                        //ensure routes are rebuilt
                         ApplicationContext.Current.ApplicationCache.RequestCache.GetCacheItem("articulate-refresh-routes", () => true);
                     }
                     break;
@@ -115,7 +122,7 @@ namespace Articulate
                     if (content == null) return;
                     if (content.ContentType.Alias.InvariantEquals("Articulate"))
                     {
-                        //add the unpublished entities to the request cache
+                        //ensure routes are rebuilt
                         UmbracoContext.Current.Application.ApplicationCache.RequestCache.GetCacheItem("articulate-refresh-routes", () => true);
                     }
                     break;                
