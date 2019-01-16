9a2e222d2f8164d183f640398fee8ebf68093494
Warren Buckley <warren@umbraco.com>
2019-01-16 16:10:25 +0000

    Try & fix up ArticulateVirtualPage

--- src/Articulate/Models/ArticulateVirtualPage.cs
+++ src/Articulate/Models/ArticulateVirtualPage.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Linq;
-using System.Web;
 using Umbraco.Core;
-using Umbraco.Core.Models;
 using Umbraco.Core.Models.PublishedContent;
 
 namespace Articulate.Models
@@ -13,6 +10,7 @@ namespace Articulate.Models
     internal class ArticulateVirtualPage : PublishedContentWrapped
     {
         private readonly string _pageName;
+        private readonly string _pageTypeAlias;
         private readonly string _urlPath;
 
         public ArticulateVirtualPage(IPublishedContent rootBlogPage, string pageName, string pageTypeAlias, string urlPath = null)
@@ -21,7 +19,7 @@ namespace Articulate.Models
             if (pageName == null) throw new ArgumentNullException("pageName");
             if (pageTypeAlias == null) throw new ArgumentNullException("pageTypeAlias");
             _pageName = pageName;
-            DocumentTypeAlias = pageTypeAlias;
+            _pageTypeAlias = pageTypeAlias;
 
             if (urlPath != null)
             {
@@ -30,25 +28,34 @@ namespace Articulate.Models
             
         }
         
-        public override string Url => base.Url.EnsureEndsWith('/') + (_urlPath ?? UrlName);
+        public override string Url => base.Url.EnsureEndsWith('/') + (_urlPath ?? base.UrlSegment);
 
         /// <summary>
         /// Returns the content that was used to create this virtual node - we'll assume this virtual node's parent is based on the real node that created it
         /// </summary>
-        public override IPublishedContent Parent => Content;
+        public override IPublishedContent Parent => base.Unwrap();
 
         public override int Id => int.MaxValue - Parent.Id;
 
         public override string Name => _pageName;
 
-        public override string UrlName => _pageName.ToLowerInvariant();
+        public override string UrlSegment => _pageName.ToLowerInvariant();
 
-        public override string DocumentTypeAlias { get; }
+        public override PublishedContentType ContentType
+        {
+            get
+            {
+                return new PublishedContentType(int.MaxValue - Parent.ContentType.Id,
+                    _pageTypeAlias,
+                    base.ContentType.ItemType, 
+                    base.ContentType.CompositionAliases, 
+                    base.ContentType.PropertyTypes, 
+                    base.ContentType.Variations);
+            }
+        }        
 
-        public override int DocumentTypeId => int.MaxValue - Parent.DocumentTypeId;
+        public override string Path => base.Unwrap().Path.EnsureEndsWith(',') + Id;
 
-        public override string Path => Content.Path.EnsureEndsWith(',') + Id;
-
-        public override int Level => Content.Level + 1;
+        public override int Level => base.Unwrap().Level + 1;
     }
 }
