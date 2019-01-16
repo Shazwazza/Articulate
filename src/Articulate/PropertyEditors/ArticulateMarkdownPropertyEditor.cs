8eea3692fa003db17a2dff2c9b85d6c1b26ec93b
Warren Buckley <warren@umbraco.com>
2019-01-16 10:13:51 +0000

    Update PropertyEditor attribute to be DataEditor

--- src/Articulate/PropertyEditors/ArticulateMarkdownPropertyEditor.cs
+++ src/Articulate/PropertyEditors/ArticulateMarkdownPropertyEditor.cs
@@ -1,11 +1,17 @@
-﻿using Umbraco.Core.PropertyEditors;
+﻿using Umbraco.Core.Logging;
+using Umbraco.Core.PropertyEditors;
 using Umbraco.Web.PropertyEditors;
 
 namespace Articulate.PropertyEditors
 {
+    //TODO: INVESTIGATE IF THIS IS FIXED IN V8 CORE - SEEMS STUPID TO HAVE TO HACK THIS
+
     //NOTE: THis is ONLY overridden because the core markdown editor is nvarchar not ntext!
-    [PropertyEditor("Articulate.MarkdownEditor", "Articulate Markdown editor", "markdowneditor", ValueType = "TEXT")]
+    [DataEditor("Articulate.MarkdownEditor", "Articulate Markdown editor", "markdowneditor", ValueType = "TEXT")]
     public class ArticulateMarkdownPropertyEditor : MarkdownPropertyEditor
     {
+        public ArticulateMarkdownPropertyEditor(ILogger logger) : base(logger)
+        {
+        }
     }
 }
