bed1e486ecec0e4edc5a5e331ac53d097abc30da
Shannon <sdeminick@gmail.com>
2020-01-31 14:22:51 +1100

    more code

--- src/Articulate/HealthChecks/TagHealthCheck.cs
+++ src/Articulate/HealthChecks/TagHealthCheck.cs
@@ -7,6 +7,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Umbraco.Core;
 using Umbraco.Core.Models;
+using Umbraco.Core.Persistence.Repositories;
+using Umbraco.Core.PropertyEditors;
+using Umbraco.Core.Scoping;
 using Umbraco.Core.Services;
 using Umbraco.Web.HealthCheck;
 
@@ -23,21 +26,92 @@ namespace Articulate.HealthChecks
         private readonly IContentService _contentService;
         private readonly IContentTypeService _contentTypeService;
         private readonly ITagService _tagService;
+        private readonly ILocalizationService _languageService;
+        private readonly IScopeProvider _scopeProvider;
+        private readonly ITagRepository _tagRepository;
 
-        public TagHealthCheck(ILocalizedTextService textService, IContentService contentService, IContentTypeService contentTypeService, ITagService tagService)
+        public TagHealthCheck(ILocalizedTextService textService, IContentService contentService, IContentTypeService contentTypeService, ITagService tagService,
+            ILocalizationService languageService, IScopeProvider scopeProvider,
+            // This is generally bad practice! but we have no choice since the services don't expose anything for us
+            ITagRepository tagRepository)
         {
             _textService = textService;
             _contentService = contentService;
             _contentTypeService = contentTypeService;
             _tagService = tagService;
+            _languageService = languageService;
+            _scopeProvider = scopeProvider;
+            _tagRepository = tagRepository;
         }
 
         public override HealthCheckStatus ExecuteAction(HealthCheckAction action)
         {
+            if (action.ActionParameters.TryGetValue("contentIds", out var ids) && ids is JObject contentIdsObj)
+            {
+                using (var scope = _scopeProvider.CreateScope())
+                {
+                    var contentIds = contentIdsObj.ToObject<Dictionary<int, string[]>>();
+
+                    var contentItems = _contentService.GetByIds(contentIds.Keys).ToList();
+
+                    foreach (var content in contentItems)
+                    {
+                        // TODO: Use SetEntityTags
+
+                        //var tagProps = contentIds[content.Id];
+                        //foreach (var tagProp in tagProps)
+                        //{
+                        //    var tagVal = content.GetValue<string>(tagProp, published: true);
+                        //    if (!tagVal.IsNullOrWhiteSpace())
+                        //    {
+                        //        var tags = tagVal.DetectIsJson()
+                        //            ? JsonConvert.DeserializeObject<string[]>(tagVal)
+                        //            : tagVal.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+
+                        //    }
+                        //}
+                    }
+
+                    scope.Complete();
+                }
+            }
+
             return new HealthCheckStatus("Tags are outta whack!")
             {
                 ResultType = StatusResultType.Success
             };
+        }
+
+
+        // Taken from Umbraco ... we need to basically do this, prob need reflection, etc...
+        private void SetEntityTags(IContentBase entity, ITagRepository tagRepo)
+        {
+            foreach (var property in entity.Properties)
+            {
+                // reflection
+                var tagConfiguration = (TagConfiguration)typeof(PropertyTagsExtensions).CallStaticMethod("GetTagConfiguration", property);
+                if (tagConfiguration == null) continue; // not a tags property
+
+                if (property.PropertyType.VariesByCulture())
+                {
+                    var tags = new List<ITag>();
+                    foreach (var pvalue in property.Values)
+                    {
+                        // reflection
+                        var tagsValue = (IEnumerable<string>)typeof(PropertyTagsExtensions).CallStaticMethod("GetTagsValue", property, pvalue.Culture);
+                        var languageId = _languageService.GetLanguageIdByIsoCode(pvalue.Culture);
+                        var cultureTags = tagsValue.Select(x => new Tag { Group = tagConfiguration.Group, Text = x, LanguageId = languageId });
+                        tags.AddRange(cultureTags);
+                    }
+                    tagRepo.Assign(entity.Id, property.PropertyType.Id, tags);
+                }
+                else
+                {
+                    var tagsValue = (IEnumerable<string>)typeof(PropertyTagsExtensions).CallStaticMethod("GetTagsValue", property, null);
+                    var tags = tagsValue.Select(x => new Tag { Group = tagConfiguration.Group, Text = x });
+                    tagRepo.Assign(entity.Id, property.PropertyType.Id, tags);
+                }
+            }
         }
 
         public override IEnumerable<HealthCheckStatus> GetStatus()
@@ -53,7 +127,7 @@ namespace Articulate.HealthChecks
             var pageIndex = 0;
             var withTagsPropertyEditors = groupedContentTypes.Where(x => x.Value.tagPropertyTypes.Count > 0).Select(x => x.Key).ToArray();
 
-            var inconsistencies = new HashSet<int>();
+            var inconsistencies = new Dictionary<int, HashSet<string>>();
 
             do
             {
@@ -69,6 +143,9 @@ namespace Articulate.HealthChecks
                         var tagVal = content.GetValue<string>(tagProp.Alias, published: true);
                         if (!tagVal.IsNullOrWhiteSpace())
                         {
+                            // TODO: use GetTagsValue with reflection
+                            // TODO: What about culture?
+
                             var tags = tagVal.DetectIsJson()
                                 ? JsonConvert.DeserializeObject<string[]>(tagVal)
                                 : tagVal.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries);
@@ -84,7 +161,10 @@ namespace Articulate.HealthChecks
                                 if (!dbTags.InvariantContains(tag))
                                 {
                                     // This means there's more tags in the text than in the DB
-                                    inconsistencies.Add(content.Id);
+                                    if (inconsistencies.TryGetValue(content.Id, out var propAliases))
+                                        propAliases.Add(tagProp.Alias);
+                                    else
+                                        inconsistencies.Add(content.Id, new HashSet<string> { tagProp.Alias });
                                     // TODO: How to deal with this?
                                 }
                             }
@@ -94,7 +174,10 @@ namespace Articulate.HealthChecks
                                 if (!tags.InvariantContains(dbTag))
                                 {
                                     // Odd, this means there's more tags in the DB than in the text!?
-                                    inconsistencies.Add(content.Id);
+                                    if (inconsistencies.TryGetValue(content.Id, out var propAliases))
+                                        propAliases.Add(tagProp.Alias);
+                                    else
+                                        inconsistencies.Add(content.Id, new HashSet<string> { tagProp.Alias });
                                     // TODO: How to deal with this?
                                 }
                             }
@@ -112,14 +195,15 @@ namespace Articulate.HealthChecks
                 new HealthCheckAction("rectify", Id)
                 {
                     Name = _textService.Localize("healthcheck/rectifyButton"),
-                    Description = $"There are {inconsistencies.Count} documents that have inconsistent tag data"
+                    ActionParameters = new Dictionary<string, object>{["contentIds"] = inconsistencies}
                 }
             };
 
-            yield return new HealthCheckStatus("Tags are outta whack!")
+            yield return new HealthCheckStatus($"There are {inconsistencies.Count} documents that have inconsistent tag data")
             {
                 ResultType = inconsistencies.Count == 0 ? StatusResultType.Success : StatusResultType.Error,
-                Actions = inconsistencies.Count == 0 ? Enumerable.Empty<HealthCheckAction>() : actions
+                Actions = inconsistencies.Count == 0 ? Enumerable.Empty<HealthCheckAction>() : actions,
+                
             };
         }
     }
