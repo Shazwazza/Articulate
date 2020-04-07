50d67b935d24cfb9aeb1f90b42cb6772b2729bb6
Shannon <sdeminick@gmail.com>
2020-04-07 10:04:25 +1000

    Fixes Content not found when enabling yyyy/mm/dd when domains are assigned #336

--- src/Articulate/Routing/DateFormattedPostContentFinder.cs
+++ src/Articulate/Routing/DateFormattedPostContentFinder.cs
@@ -15,46 +15,47 @@ namespace Articulate.Routing
 
         public override bool TryFindContent(PublishedRequest contentRequest)
         {
-            string route;
-            if (contentRequest.HasDomain)
-                route = contentRequest.Domain.ContentId.ToString() + DomainUtilities.PathRelativeToDomain(contentRequest.Domain.Uri, contentRequest.Uri.GetAbsolutePathDecoded());
-            else
-                route = contentRequest.Uri.GetAbsolutePathDecoded();
-
+            
             // This simple logic should do the trick: basically if I find an url with more than 4 segments (the 3 date parts and the slug)
             // I leave the last segment (the slug), remove the 3 date parts, and keep all the rest.
             var segmentLength = contentRequest.Uri.Segments.Length;
-            if (segmentLength > 4)
+            if (segmentLength <= 4) return false;
+            
+            var stringDate = contentRequest.Uri.Segments[segmentLength - 4] + contentRequest.Uri.Segments[segmentLength - 3] + contentRequest.Uri.Segments[segmentLength - 2].TrimEnd("/");
+            DateTime postDate;
+            try
             {
-                var stringDate = contentRequest.Uri.Segments[segmentLength - 4] + contentRequest.Uri.Segments[segmentLength - 3] + contentRequest.Uri.Segments[segmentLength - 2].TrimEnd("/");
-                DateTime postDate;
-                try
-                {
-                    postDate = DateTime.ParseExact(stringDate, "yyyy/MM/dd", CultureInfo.InvariantCulture);
-                }
-                catch (FormatException)
-                {
-                    return false;
-                }
-
-                var newRoute = string.Empty;
-                for (int i = 0; i < segmentLength; i++)
-                {
-                    if (i < segmentLength - 4 || i > segmentLength - 2)
-                        newRoute += contentRequest.Uri.Segments[i];
-                }
-                var node = FindContent(contentRequest, newRoute);
-                contentRequest.PublishedContent = null;
-                // If by chance something matches the format pattern I check again if there is sucn a node and if it's an articulate post
-                if (node == null || (node.ContentType.Alias != "ArticulateRichText" && node.ContentType.Alias != "ArticulateMarkdown")) return false;
-                if (!node.Parent.Parent.Value<bool>("useDateFormatForUrl")) return false;
-                if (node.Value<DateTime>("publishedDate").Date != postDate.Date) return false;
-
-                contentRequest.PublishedContent = node;
-                return true;
+                postDate = DateTime.ParseExact(stringDate, "yyyy/MM/dd", CultureInfo.InvariantCulture);
+            }
+            catch (FormatException)
+            {
+                return false;
             }
 
-            return false;
+            var newRoute = string.Empty;
+            for (var i = 0; i < segmentLength; i++)
+            {
+                if (i < segmentLength - 4 || i > segmentLength - 2)
+                    newRoute += contentRequest.Uri.Segments[i].ToLowerInvariant();
+            }
+
+            // if there's a domain attached we need to lookup the content with the domain Id
+            // and the domain's path stripped from the start
+            if (contentRequest.HasDomain)
+            {
+                newRoute = contentRequest.Domain.ContentId + DomainUtilities.PathRelativeToDomain(contentRequest.Domain.Uri, newRoute);
+            }
+
+            var node = FindContent(contentRequest, newRoute);
+            contentRequest.PublishedContent = null;
+            // If by chance something matches the format pattern I check again if there is sucn a node and if it's an articulate post
+            if (node == null || (node.ContentType.Alias != "ArticulateRichText" && node.ContentType.Alias != "ArticulateMarkdown")) return false;
+            if (!node.Parent.Parent.Value<bool>("useDateFormatForUrl")) return false;
+            if (node.Value<DateTime>("publishedDate").Date != postDate.Date) return false;
+
+            contentRequest.PublishedContent = node;
+            return true;
+
         }
     }
 }
