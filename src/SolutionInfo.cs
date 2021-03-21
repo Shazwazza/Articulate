c78c4e75fe3e82c2b20595164624982cf2ca6aac
Shannon <sdeminick@gmail.com>
2021-03-22 10:26:25 +1100

    gets auto copyright working and improves build

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -2,8 +2,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyCopyright("Copyright © Shannon Deminick 2021")]
-
 [assembly: AssemblyCompany("Shannon Deminick")]
 
 [assembly: AssemblyConfiguration("")]
