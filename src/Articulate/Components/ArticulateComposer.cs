d64864c44f025308485fe877cf868a5f6328c963
Warren Buckley <warren@umbraco.com>
2022-08-02 13:44:52 +0100

    From composer remove reference to MetaWeblog Route handler & factory

--- src/Articulate/Components/ArticulateComposer.cs
+++ src/Articulate/Components/ArticulateComposer.cs
@@ -1,18 +1,16 @@
 using Articulate.ImportExport;
+using Articulate.MetaWeblog;
 using Articulate.Options;
 using Articulate.Packaging;
 using Articulate.Routing;
 using Articulate.Services;
 using Articulate.Syndication;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.AspNetCore.Routing.Matching;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
 using Umbraco.Cms.Core.Notifications;
 using Umbraco.Cms.Core.Routing;
-using static Microsoft.Extensions.DependencyInjection.ServiceDescriptor;
+using WilderMinds.MetaWeblog;
 
 namespace Articulate.Components
 {
@@ -41,15 +39,7 @@ namespace Articulate.Components
             services.AddSingleton<ArticulateRouteValueTransformer>();
             services.AddSingleton<ArticulateRouter>();
             //services.TryAddEnumerable(Singleton<MatcherPolicy, ArticulateSelectorPolicy>());
-            //services.AddSingleton<ArticulateEndpointDataSource>();
-
-            //services.AddTransient<MetaWeblogHandler>();
-            // services.AddSingleton<MetaWeblogHandlerFactory>(factory => new MetaWeblogHandlerFactory(i =>
-            //{
-            //    var instance = factory.GetRequiredService<MetaWeblogHandler>();
-            //    instance.BlogRootId = i;
-            //    return instance;
-            //}));
+            //services.AddSingleton<ArticulateEndpointDataSource>();           
 
             builder.UrlProviders().Append<VirtualNodeUrlProvider>();
             builder.UrlProviders().InsertBefore<DefaultUrlProvider, DateFormattedUrlProvider>();
