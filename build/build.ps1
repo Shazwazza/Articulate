349cc72dc9f50fdb06ef323a0c564ce47c553145
Gavin Faux <gavin.faux@reddev.co.uk>
2025-10-25 02:50:02 +0100

    Updates build process and dependencies
    Modernizes the build workflow by updating GitHub Actions and switching to dotnet CLI.
    
    This change streamlines the build process, improves dependency management, and ensures compatibility with the latest .NET SDK features.

--- build/build.ps1
+++ build/build.ps1
@@ -6,35 +6,6 @@ $ReleaseFolder = Join-Path -Path $BuildFolder -ChildPath "Release";
 $SolutionRoot = Join-Path -Path $RepoRoot "src";
 $CodeProjFolder = Join-Path -Path $RepoRoot -ChildPath "src\Articulate";
 $CodeCSProj = Join-Path -Path $CodeProjFolder -ChildPath "Articulate.csproj";
-
-# Go get nuget.exe if we don't hae it
-$NuGet = "$BuildFolder\nuget.exe"
-$FileExists = Test-Path $NuGet 
-If ($FileExists -eq $False) {
-	Write-Host "Retrieving nuget.exe..."
-	$SourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
-	Invoke-WebRequest $SourceNugetExe -OutFile $NuGet
-}
-
-# ensure we have vswhere
-New-Item "$BuildFolder\vswhere" -type directory -force
-$vswhere = "$BuildFolder\vswhere.exe"
-if (-not (test-path $vswhere))
-{
-	Write-Host "Download VsWhere..."
-    $path = "$ToolsFolder\tmp"
-    &$nuget install vswhere -OutputDirectory $path
-    $dir = ls "$path\vswhere.*" | sort -property Name -descending | select -first 1
-    $file = ls -path "$dir" -name vswhere.exe -recurse
-    mv "$dir\$file" $vswhere   
-}
-
-$MSBuild = &$vswhere -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe | select-object -first 1
-if (-not (test-path $MSBuild)) {
-    throw "MSBuild not found!"
-}
-
-
 if ((Get-Item $ReleaseFolder -ErrorAction SilentlyContinue) -ne $null)
 {
 	Write-Warning "$ReleaseFolder already exists on your local machine. It will now be deleted."
@@ -43,26 +14,37 @@ if ((Get-Item $ReleaseFolder -ErrorAction SilentlyContinue) -ne $null)
 
 ####### DO THE SLN BUILD PART #############
 
+# Get the solution path
+$SolutionPath = Join-Path -Path $SolutionRoot -ChildPath "Articulate.sln"
+
 # Build the solution in release mode
-$SolutionPath = Join-Path -Path $SolutionRoot -ChildPath "Articulate.sln";
 
-#restore nuget packages
+dotnet --version
+
+# Restore packages
 Write-Host "Restoring nuget packages..."
-& $NuGet restore $SolutionPath
-
-# clean sln for all deploys
-& $MSBuild "$SolutionPath" /p:Configuration=Release /maxcpucount /t:Clean
-if (-not $?)
-{
-	throw "The MSBuild process returned an error code."
+& dotnet restore $SolutionPath
+if (-not $?) {
+    throw "The dotnet restore process returned an error code."
 }
 
-#build
-& $MSBuild "$SolutionPath" /p:Configuration=Release /maxcpucount
-if (-not $?)
-{
-	throw "The MSBuild process returned an error code."
+# Clean solution
+Write-Host "Cleaning solution..."
+& dotnet clean $SolutionPath --configuration Release
+if (-not $?) {
+    throw "The dotnet clean process returned an error code."
 }
 
-# dotnet pack (As its a SDK style project, nuget pack was not reading info stored in csproj)
-& dotnet pack $CodeCSProj --output $ReleaseFolder --configuration Release
+# Build solution
+Write-Host "Executing dotnet build"
+& dotnet build $SolutionPath --configuration Release --no-restore
+if (-not $?) {
+    throw "The dotnet build process returned an error code."
+}
+
+# dotnet pack
+Write-Host "Packing Articulate..."
+& dotnet pack $SolutionPath --output $ReleaseFolder --configuration Release
+if (-not $?) {
+    throw "The dotnet pack process returned an error code."
+}
