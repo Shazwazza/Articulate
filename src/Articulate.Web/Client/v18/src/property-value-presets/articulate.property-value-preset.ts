cf30cd2c84cf4d9502ed3226ae290f9e70f4d98b
Gavin Faux <gavin.faux@reddev.co.uk>
2026-06-20 05:34:28 +0100

    fix(client): default blog root theme to Material in backoffice preset
    - align property-value-preset.ts with package.zip starter content
      (both now default to Material; preset previously returned VAPOR)
    - delete dead code path src\packages\articulate-markdown-editor\umbraco-package.ts

--- src/Articulate.Web/Client/v18/src/property-value-presets/articulate.property-value-preset.ts
+++ src/Articulate.Web/Client/v18/src/property-value-presets/articulate.property-value-preset.ts
@@ -13,7 +13,7 @@ export class ArticulateBlogPresetApi implements UmbPropertyValuePreset {
 
     switch (callArgs.alias) {
       case 'theme':
-        return 'VAPOR';
+        return 'Material';
       case 'pageSize':
         return 10;
       case 'categoriesUrlName':
